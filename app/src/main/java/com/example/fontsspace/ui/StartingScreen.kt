8a065bd45207ff744b7d7e95c516729c85c7daf5
101919950+kiranmkr@users.noreply.github.com <117913744+amantausif2003@users.noreply.github.com>
2023-02-21 22:01:23 +0500

    under_dev

---
+++ app/src/main/java/com/example/fontsspace/ui/StartingScreen.kt
@@ -0,0 +1,32 @@
+package com.example.fontsspace.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.example.fontsspace.R
+import com.example.fontsspace.databinding.ActivityMainBinding
+import com.example.fontsspace.databinding.StartingScreenBinding
+
+class StartingScreen : AppCompatActivity() {
+
+    private lateinit var mainBinding: StartingScreenBinding
+
+    private var workerHandler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        mainBinding = StartingScreenBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        Log.d("myApplication","onCreate SplashScreen")
+        workerHandler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },1000)
+
+    }
+}
